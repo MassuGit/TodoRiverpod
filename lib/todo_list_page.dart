d2e82b218f30b5933958046746b9e7a17b63d9b1
Massu <rmmastercoding@gmail.com>
2024-03-08 21:53:47 +0900

    feat: created the foundation for the todo list page

---
+++ lib/todo_list_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(
+    ProviderScope(child: TodoApp()),
+  );
+}
+
+final homeTextProvider = Provider((ref) => 'Hello xiaoting!');
+
+class TodoApp extends ConsumerWidget {
+  TodoApp({super.key});
+
+  final todoList = [
+    "todo1",
+    "todo10",
+    "todo15",
+    "todo100",
+    "todo150",
+    "todo200",
+    "todo230",
+    "todo300",
+    "todo500",
+  ];
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final homeText = ref.read(homeTextProvider);
+
+    return MaterialApp(
+      title: "Title",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(homeText),
+        ),
+        body: ListView.builder(
+          itemBuilder: (BuildContext context, index) {
+            if (index < todoList.length) {
+              return _TodoItem(todoTitle: todoList[index]);
+            }
+            return null;
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _TodoItem extends StatelessWidget {
+  const _TodoItem({required this.todoTitle});
+
+  final String todoTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(18.0),
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: Colors.black),
+        ),
+      ),
+      child: Text(
+        todoTitle,
+        style: const TextStyle(fontSize: 24.0),
+      ),
+    );
+  }
+}
