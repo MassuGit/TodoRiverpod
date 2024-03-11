0184141fd640fca6478b0ae82577c50f61dcca76
Massu <rmmastercoding@gmail.com>
2024-03-12 00:04:16 +0900

    fix: fixed directory and created new directory UI

--- lib/router.dart
+++ lib/router.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:todo_riverpod/add_todo_page.dart';
-import 'package:todo_riverpod/todo_list_page.dart';
+import 'package:todo_riverpod/ui/add_todo_page.dart';
+import 'package:todo_riverpod/ui/todo_list_page.dart';
 
 final router = GoRouter(
   routes: <RouteBase>[
